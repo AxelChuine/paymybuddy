5a1e076dc0e551930814c519c86936bff7562ba2
axel_chuine_dev <axel.chuine.pro@gmail.com>
2024-04-22 12:36:44 +0200

    login page created

--- src/main/java/com/openclassrooms/paymybuddy/controller/HomeController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.paymybuddy.controller;
 
+import com.openclassrooms.paymybuddy.service.IPersonService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping
 public class HomeController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String login() {
+    private final IPersonService personService;
+
+    public HomeController(IPersonService personService) {
+        this.personService = personService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String login(@ModelAttribute("email") String email, @ModelAttribute("password") String password, Model model) {
         return "login";
     }
 }
