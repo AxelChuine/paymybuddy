cfe19c4798a8208d6fc05fc7be89894bcdb03759
Axel CHUINE <axel.chuine.pro@gmail.com>
2024-05-04 17:38:28 +0200

    Problème dans le test payMyBuddyShouldReturnATransaction

--- src/main/java/com/openclassrooms/paymybuddy/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/impl/TransactionServiceImpl.java
@@ -1,11 +1,15 @@
 package com.openclassrooms.paymybuddy.service.impl;
 
+import com.openclassrooms.paymybuddy.model.Transaction;
 import com.openclassrooms.paymybuddy.repository.ITransactionRepository;
+import com.openclassrooms.paymybuddy.service.IAccountService;
 import com.openclassrooms.paymybuddy.service.ITransactionService;
+import com.openclassrooms.paymybuddy.service.dto.AccountDTO;
 import com.openclassrooms.paymybuddy.service.dto.TransactionDTO;
 import com.openclassrooms.paymybuddy.service.mapper.ITransactionMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -13,12 +17,28 @@ public class TransactionServiceImpl implements ITransactionService {
 
     private final ITransactionRepository repository;
 
-    public TransactionServiceImpl(ITransactionRepository repository) {
+    private final IAccountService accountService;
+
+    public TransactionServiceImpl(ITransactionRepository repository, IAccountService accountService) {
         this.repository = repository;
+        this.accountService = accountService;
     }
 
     @Override
     public List<TransactionDTO> findAllByAccountId(Integer accountId) {
         return ITransactionMapper.INSTANCE.transactionsToTransactionsDtos(this.repository.findAllByAccountId(accountId));
+    }
+
+    @Override
+    public TransactionDTO payMyBuddy(Integer pSender, String emailReceiver, Float amount) {
+        AccountDTO sender = this.accountService.findById(pSender);
+        AccountDTO receiver = this.accountService.findByEmail(emailReceiver);
+        Float balanceSender = sender.getBalance() - amount;
+        Float balanceReceiver = receiver.getBalance() + amount;
+        sender.setBalance(balanceSender);
+        receiver.setBalance(balanceReceiver);
+        this.accountService.save(sender);
+        this.accountService.save(receiver);
+        return ITransactionMapper.INSTANCE.transactionToTransactionDto(this.repository.save(new Transaction(amount, sender.getIdentifier(), receiver.getIdentifier(), LocalDateTime.now())));
     }
 }
--- src/test/java/com/openclassrooms/paymybuddy/service/impl/TransactionServiceTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/service/impl/TransactionServiceTest.java
@@ -2,14 +2,19 @@ package com.openclassrooms.paymybuddy.service.impl;
 
 import com.openclassrooms.paymybuddy.model.Transaction;
 import com.openclassrooms.paymybuddy.repository.ITransactionRepository;
+import com.openclassrooms.paymybuddy.service.IAccountService;
+import com.openclassrooms.paymybuddy.service.dto.AccountDTO;
 import com.openclassrooms.paymybuddy.service.dto.TransactionDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,13 +26,27 @@ public class TransactionServiceTest {
     @Mock
     private ITransactionRepository repository;
 
+    @Mock
+    private IAccountService accountService;
+
     @InjectMocks
     private TransactionServiceImpl service;
+
+    private Transaction transaction;
+
+    private TransactionDTO transactionDTO;
+
+    private AccountDTO sender;
+
+    private AccountDTO recipient;
 
 
     @BeforeEach
     public void setTransaction () {
-        Transaction transaction = new Transaction();
+        transaction = new Transaction( 50.0F, 1, 2, LocalDateTime.now());
+        transactionDTO = new TransactionDTO(50.0F, 1, 2, LocalDateTime.now());
+        this.sender = new AccountDTO(1, null, null, null, null, null, 100.0F, null);
+        this.recipient = new AccountDTO(1, null, null, null, null, null, 100.0F, null);
     }
 
     @Test
@@ -40,5 +59,22 @@ public class TransactionServiceTest {
         List<TransactionDTO> transactionDtoToCompare = this.service.findAllByAccountId(accountId);
 
         assertEquals(transactionDtoList, transactionDtoToCompare);
+    }
+
+    @Test
+    public void payMyBuddyShouldReturnATransaction () {
+        String emailReceiver = "test@test.com";
+        Integer identifier = 1;
+        AccountDTO senderToSave = new AccountDTO(1, null, null, null, null, null, 50.0F, null);
+        AccountDTO recipientToSave = new AccountDTO(1, null, null, null, null, null, 150.0F, null);
+
+        Mockito.when(this.accountService.findById(identifier)).thenReturn(this.sender);
+        Mockito.when(this.accountService.findByEmail(emailReceiver)).thenReturn(this.recipient);
+        Mockito.when(this.repository.save(this.transaction)).thenReturn(transaction);
+        Mockito.when(this.accountService.save(senderToSave)).thenReturn(senderToSave);
+        Mockito.when(this.accountService.save(recipientToSave)).thenReturn(recipientToSave);
+        TransactionDTO transactionToCompare = this.service.payMyBuddy(this.transaction.getSenderId(), emailReceiver, this.transaction.getAmount());
+
+        Assertions.assertEquals(this.transactionDTO, transactionToCompare);
     }
 }
