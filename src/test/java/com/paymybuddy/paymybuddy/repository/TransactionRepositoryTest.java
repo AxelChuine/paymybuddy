2457b5c05c3653f3c4d1814f7999f8286d9817dd
AxelCh <axel.chuine.pro@gmail.com>
2024-12-29 19:00:54 +0100

    test de transactionrepository qui marche

---
+++ src/test/java/com/paymybuddy/paymybuddy/repository/TransactionRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.paymybuddy.paymybuddy.repository;
+
+import com.paymybuddy.paymybuddy.models.Transaction;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class TransactionRepositoryTest {
+    @Autowired
+    private ITransactionRepository repository;
+
+    @Test
+    public void findAllShouldReturnAllTransactions() {
+        List<Transaction> transactions = repository.findAll();
+
+        assertThat(transactions).isNotNull();
+        assertThat(transactions.getFirst()).isNotNull();
+    }
+}
