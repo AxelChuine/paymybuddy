d48d6b0611c92b0271453d930d448f3c485d2167
Axel CHUINE <axel.chuine.pro@gmail.com>
2025-01-24 12:58:32 +0100

    ajout de l'exception pour la relation pas trouvée

--- src/main/java/com/paymybuddy/paymybuddy/exceptions/ConnectionNotFoundException.java
+++ src/main/java/com/paymybuddy/paymybuddy/exceptions/ConnectionNotFoundException.java
@@ -1,4 +1,12 @@
 package com.paymybuddy.paymybuddy.exceptions;
 
-public class ConnectionNotFoundException {
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+
+@Data
+@EqualsAndHashCode(callSuper=true)
+public class ConnectionNotFoundException extends Exception {
+    private final String message = "Connection not found";
+    private final HttpStatus status = HttpStatus.NOT_FOUND;
 }
