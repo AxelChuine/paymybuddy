b9e44532783e70cb4adc47f3c0d2d7e04893cdf8
Axel CHUINE <axel.chuine.pro@gmail.com>
2025-01-24 12:51:57 +0100

    ajout de la méthode pour trouver les relations de l'utilisateur actuel

---
+++ src/main/java/com/paymybuddy/paymybuddy/dtos/TransactionToDisplay.java
@@ -0,0 +1,4 @@
+package com.paymybuddy.paymybuddy.dtos;
+
+public class TransactionToDisplay {
+}
--- src/main/java/com/paymybuddy/paymybuddy/repository/IConnectionRepository.java
+++ src/main/java/com/paymybuddy/paymybuddy/repository/IConnectionRepository.java
@@ -1,10 +1,17 @@
 package com.paymybuddy.paymybuddy.repository;
 
+import com.paymybuddy.paymybuddy.dtos.ConnectionVM;
+import com.paymybuddy.paymybuddy.models.Account;
 import com.paymybuddy.paymybuddy.models.Connection;
 import com.paymybuddy.paymybuddy.models.ConnectionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IConnectionRepository extends JpaRepository<Connection, ConnectionId> {
+    Connection findByConnection(Account account);
+
+    List<Connection> findAllByAccount(Account account);
 }
